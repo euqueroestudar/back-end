18b29abc8f22b409733ab263d6430f64eb744ffe
Lucas P. Sodré <llucasbrasill@gmail.com>
2021-10-31 22:53:41 -0300

    feat: add prisma service

--- src/institution/institution.module.ts
+++ src/institution/institution.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { InstitutionService } from './institution.service';
 
 @Module({
-  providers: [InstitutionService]
+  providers: [InstitutionService, PrismaService],
 })
 export class InstitutionModule {}
